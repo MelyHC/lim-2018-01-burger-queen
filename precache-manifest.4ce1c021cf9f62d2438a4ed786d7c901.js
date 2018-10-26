799fa23fd012b42ce5abb19267a0df155fbc1e0d
MelyHC <mely.hidalgo.crespo@gmail.com>
2018-10-26 18:33:20 -0500

    Updates

---
+++ precache-manifest.4ce1c021cf9f62d2438a4ed786d7c901.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0dab808f1c361c40aacb",
+    "url": "/lim-2018-01-burger-queen/static/js/runtime~main.0dab808f.js"
+  },
+  {
+    "revision": "54bef377aea64ca43b1c",
+    "url": "/lim-2018-01-burger-queen/static/js/main.54bef377.chunk.js"
+  },
+  {
+    "revision": "9beb4995ed3f7d5a31c9",
+    "url": "/lim-2018-01-burger-queen/static/js/1.9beb4995.chunk.js"
+  },
+  {
+    "revision": "54bef377aea64ca43b1c",
+    "url": "/lim-2018-01-burger-queen/static/css/main.71cbc052.chunk.css"
+  },
+  {
+    "revision": "9beb4995ed3f7d5a31c9",
+    "url": "/lim-2018-01-burger-queen/static/css/1.407de6ee.chunk.css"
+  },
+  {
+    "revision": "cc821fa5e4ce3109b37877a81a55b617",
+    "url": "/lim-2018-01-burger-queen/index.html"
+  }
+];
